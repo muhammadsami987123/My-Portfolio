9898f0472035c2302b1cb109c5c1570681919a56
muhammadsami987123 <160221069+muhammadsami987123@users.noreply.github.com>
2024-11-09 14:56:40 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";``
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      easing:"ease-out-back",
+      duration:1200,
+      delay: 100,
+      mirror : true,
+      anchorPlacement:"bottom-bottom",
+      offset:160,
+    });
+    AOS.refresh();
+  } , []);
+  return (
+          <main>
+            <Hero/>
+            <Projects/>
+            <Skills/>
+            <Contact/>
+            <About/>
+          </main>
+  );
+}
